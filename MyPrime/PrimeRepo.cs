8c70cb7c42131ca3a9f51135359e47bb2117ffd3
Jeff Zhou <3722845+prime167@users.noreply.github.com>
2020-01-22 10:35:58 +0800

    .

--- MyPrime/PrimeRepo.cs
+++ MyPrime/PrimeRepo.cs
@@ -2,9 +2,6 @@
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Dapper;
 using Dapper.Contrib.Extensions;
 
